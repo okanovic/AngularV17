0a867ba33e59febdab3bb05704ec02923e5a0c93
EXT02D53873 <okan.toper@consultant.turkcell.com.tr>
2023-12-09 21:33:39 +0300

    sidebar component oluşturuldu

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { LayoutRoutingModule } from './layout.routes.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent,SidebarComponent],
   imports: [CommonModule, LayoutRoutingModule],
-  exports: [LayoutComponent],
+  exports: [LayoutComponent,SidebarComponent],
 })
 export class LayoutModule {}
